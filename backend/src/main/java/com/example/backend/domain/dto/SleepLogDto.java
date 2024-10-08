65616947e1ce2132534d758d94c0d84d29fc5eae
Rajan66 <rajanmaharjan042@gmail.com>
2024-10-08 20:01:50 +0545

    feat: backend setup

---
+++ backend/src/main/java/com/example/backend/domain/dto/SleepLogDto.java
@@ -0,0 +1,23 @@
+package com.example.backend.domain.dto;
+
+
+import com.example.backend.domain.entities.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SleepLogDto {
+    private Long id;
+    private UserEntity user_id;
+    private LocalDateTime sleep_start;
+    private LocalDateTime wake_time;
+    private Long quality_rating;
+    private String notes;
+}
