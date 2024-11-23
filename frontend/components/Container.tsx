c5191bb723cee17e94beb1feb1721398c955d015
Rajan66 <rajanmaharjan042@gmail.com>
2024-11-23 11:00:04 +0545

    test undo

--- frontend/components/Container.tsx
+++ frontend/components/Container.tsx
@@ -6,7 +6,6 @@ export default function Container({ children }: ContainerProps) {
     return (
         <div className="max-w-[1400px] mx-auto px-4 md:px-10 xl:px-28 h-full">
             {children}
-            hei
         </div>
     );
 }
